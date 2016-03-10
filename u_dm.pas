6e5b0f54a734eaa6506c7574685833ce91762fe9
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-03-10 19:10:16 +0700

    app path
    add app path
    re format code.

--- u_dm.pas
+++ u_dm.pas
@@ -4,8 +4,8 @@ interface
 
 uses
   SysUtils, Classes, DB, mySQLDbTables, sSkinManager, frxClass, frxDBSet,
-  frxExportXML, dialogs,forms,inifiles,u_fungsi, ImgList, Controls,
-  acAlphaImageList, frxBarcode,Windows, cxStyles;
+  frxExportXML, dialogs, forms, inifiles, u_fungsi, ImgList, Controls,
+  acAlphaImageList, frxBarcode, Windows, cxStyles, SHFolder;
 
 type
   Tdm = class(TDataModule)
@@ -48,24 +48,33 @@ type
   private
     { Private declarations }
   public
-  a_path:string;
+    a_path, AppPath: string;
     { Public declarations }
   end;
 
 var
   dm: Tdm;
-  fungsi:tfungsi;
-  pilihan:integer;
-  kd_comp,kd_operator,n_operator:string;
-  metu_kabeh:Boolean;
-  Mgs : TMsg;
-
+  fungsi: tfungsi;
+  pilihan: integer;
+  kd_comp, kd_operator, n_operator: string;
+  metu_kabeh: Boolean;
+  Mgs: TMsg;
 
 implementation
 
 {$R *.dfm}
 
-function krupuk(const s: String; CryptInt: Integer): String;
+function GetAppData(Folder: Integer): string;
+var
+  path: array[0..MAX_PATH] of Char;
+begin
+  if Succeeded(SHGetFolderPath(0, Folder, 0, 0, @Path[0])) then
+    Result := path + '\Gain Profit\'
+  else
+    Result := '';
+end;
+
+function krupuk(const s: string; CryptInt: Integer): string;
 var
   i: integer;
   s2: string;
@@ -78,61 +87,67 @@ end;
 
 procedure Tdm.DataModuleCreate(Sender: TObject);
 var
-  data,pusat,jalur1,jalur2,nama,kata: string;
+  data, pusat, jalur1, jalur2, nama, kata: string;
   X: TextFile;
-  appINI : TIniFile;
+  appINI: TIniFile;
 begin
-  a_path:= extractfilepath(application.ExeName);
-sm.SkinDirectory:=a_path+'\tools\skins';
+  a_path := extractfilepath(application.ExeName);
 
-  appINI := TIniFile.Create(a_path+'\tools\gain.ini') ;
- try
-  kd_comp := appINI.ReadString ('toko', 'kd_perusahaan', '');
-  sm.SkinName:=appINI.ReadString('toko','nama_skin','Acryl (internal)');
-  sm.HueOffset:=appini.ReadInteger('toko','hue_skin',0);
-  sm.Saturation:=appini.ReadInteger('toko','sat_skin',0);
- finally
- appINI.Free;
- end;
+  AppPath := GetAppData(CSIDL_COMMON_APPDATA);
+  if not (DirectoryExists(AppPath)) then
+    CreateDir(AppPath);
+  sm.SkinDirectory := a_path + '\tools\skins';
 
- sm.Active:=true;
+  appINI := TIniFile.Create(a_path + '\tools\gain.ini');
+  try
+    kd_comp := appINI.ReadString('toko', 'kd_perusahaan', '');
+    sm.SkinName := appINI.ReadString('toko', 'nama_skin', 'Acryl (internal)');
+    sm.HueOffset := appini.ReadInteger('toko', 'hue_skin', 0);
+    sm.Saturation := appini.ReadInteger('toko', 'sat_skin', 0);
+  finally
+    appINI.Free;
+  end;
 
- db_conn.Connected:= False;
- 
- assignfile(X,'tools\koneksi.cbCon');
- try
-   reset(X);
-   readln(X,pusat);
-   readln(X,data);
-   readln(X,jalur2);
-   readln(X,nama);
-   readln(X,kata);
-   closefile(X);
- db_conn.Host :=krupuk(pusat,6);
- db_conn.DatabaseName:= krupuk(data,6);
- jalur1 :=krupuk(jalur2,6);
- db_conn.Port :=strtoint(jalur1);
- db_conn.UserName :=krupuk(nama,6);
- db_conn.UserPassword :=krupuk(kata,6);
- db_conn.Connected:= true;
- 
- except
- showmessage('koneksi tidak berhasil');
- application.Terminate;
- end;
+  sm.Active := true;
+
+  db_conn.Connected := False;
+
+  assignfile(X, 'tools\koneksi.cbCon');
+  try
+    reset(X);
+    readln(X, pusat);
+    readln(X, data);
+    readln(X, jalur2);
+    readln(X, nama);
+    readln(X, kata);
+    closefile(X);
+    db_conn.Host := krupuk(pusat, 6);
+    db_conn.DatabaseName := krupuk(data, 6);
+    jalur1 := krupuk(jalur2, 6);
+    db_conn.Port := strtoint(jalur1);
+    db_conn.UserName := krupuk(nama, 6);
+    db_conn.UserPassword := krupuk(kata, 6);
+    db_conn.Connected := true;
+
+  except
+    showmessage('koneksi tidak berhasil');
+    application.Terminate;
+  end;
 end;
 
 procedure Tdm.smAfterChange(Sender: TObject);
-  var appINI : TIniFile;
+var
+  appINI: TIniFile;
 begin
-  appINI := TIniFile.Create(dm.a_path+'\tools\gain.ini') ;
-    try
-      appINI.WriteString('toko','nama_skin',dm.sm.SkinName);
-      appINI.WriteInteger('toko','hue_skin',dm.sm.HueOffset);
-      appINI.WriteInteger('toko','sat_skin',dm.sm.Saturation);
-    finally
-      appINI.Free;
-    end;
+  appINI := TIniFile.Create(dm.a_path + '\tools\gain.ini');
+  try
+    appINI.WriteString('toko', 'nama_skin', dm.sm.SkinName);
+    appINI.WriteInteger('toko', 'hue_skin', dm.sm.HueOffset);
+    appINI.WriteInteger('toko', 'sat_skin', dm.sm.Saturation);
+  finally
+    appINI.Free;
+  end;
 end;
 
 end.
+
